b943bae9937a310c219a6072242f6b0abfeafdc9
DIEGO RODRIGUES DUARTE <diegorodrigues4544@gmail.com>
2025-03-18 10:13:30 -0300

    update: variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,32 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Estudar tipos primitivos.
+        // Estudar classe String que representa texto na aplicação.
+
+        String meuNome = "Diego Rodrigues";
+
+        double salarioMinino = 2500.03; 
+
+        short numeroCurto = 1;
+        int numeroNormal1 = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal1;
+
+        /** 
+         Short entre () é um cast que tranforma um int em short para caber na variavel short
+         Estou passando um valor int em uma variavel short que nao caberia por isso uso do 'cast'.
+         para garantir que o valor ira caber.
+        */ 
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        //A palavra reservada final garante que a variavel PI nunca ira receber outro valor.
+        final double PI = 3.14;
+
+        //Por convenção toda variavel com final deve ser escrita em CAIXA ALTA.
+
+
     }
 }
