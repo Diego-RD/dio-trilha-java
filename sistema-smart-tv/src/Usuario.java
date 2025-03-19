8db48db205adad77dab2843fbc683f0835eb6d69
DIEGO RODRIGUES DUARTE <diegorodrigues4544@gmail.com>
2025-03-19 15:11:30 -0300

    desafio: SmartTv

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,26 @@
+public class Usuario {
+
+    public static void main(String[] args) throws Exception {
+        
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();   
+
+        smartTv.mudarCanal(13);
+        System.out.println("Volume Atual? :" +smartTv.volume);
+
+        System.out.println("TV esta ligada? :" + smartTv.ligada);
+        System.out.println("Canal Autal? :" +smartTv.canal);
+        System.out.println("Volume Atual? :" +smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo status -> TV esta ligada? :" + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo status -> TV esta ligada? :" + smartTv.ligada);
+
+    }
+}
