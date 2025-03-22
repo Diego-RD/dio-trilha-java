6d207e59cd455370f704a478a940948abc0cbfa3
DIEGO RODRIGUES DUARTE <diegorodrigues4544@gmail.com>
2025-03-22 10:28:15 -0300

    Update: estrutura de condições

---
+++ controle-fluxo/src/CaixaEletronico.java
@@ -0,0 +1,13 @@
+public class CaixaEletronico {
+    public static void main(String[] args) {
+        double saldo = 25.0;
+        double valorSolicidado = 22.0;
+
+        if (valorSolicidado < saldo){
+            saldo = saldo - valorSolicidado;
+            System.out.println("Novo saldo " + saldo);
+        }else
+            System.out.println("Saldo insuficiente");
+
+    }
+}
